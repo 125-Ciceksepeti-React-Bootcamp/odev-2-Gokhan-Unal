94b6a88973d38abb84be00d5cbe7410dd0211f3a
Gökhan Ünal <unal_gokhan@outlook.com>
2021-09-23 00:13:19 +0300

    babel

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,15 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
         test: /\.html$/,
         use: [
           {
